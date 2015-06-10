803953969257029b932e42d41e7d0218f1880c4e
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2015-06-10 12:36:53 -0500

    cambios estáticos
    se aumentaro sliders para otros manejos, y solo se hicieron cambios de
    potencia para poder ir viendo como podría quedar mejor, falta hacer una
    mejor interactividad con el usuario.

--- public/js/slider.js
+++ public/js/slider.js
@@ -10,5 +10,23 @@
     cambio.onchange = function() {
       document.getElementById('change2').innerHTML = cambio.value;
     };
+    var cambio = document.querySelector('#chan3')
+      , initcambio = new Powerange(cambio, { start: 50 });
+
+    cambio.onchange = function() {
+      document.getElementById('change3').innerHTML = cambio.value;
+    };
+    var cambio = document.querySelector('#chan4')
+      , initcambio = new Powerange(cambio, { start: 50 });
+
+    cambio.onchange = function() {
+      document.getElementById('change4').innerHTML = cambio.value;
+    };
+    var cambio = document.querySelector('#chan5')
+      , initcambio = new Powerange(cambio, { start: 50 });
+
+    cambio.onchange = function() {
+      document.getElementById('change5').innerHTML = cambio.value;
+    };
 
 
