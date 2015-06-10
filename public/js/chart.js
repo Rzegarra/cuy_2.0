8ea917bc0f7b5e087f301fa2acb7cd0a6d54fe62
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2015-06-10 12:17:23 -0500

    :)
    creo q se mira mejor así, se uso un widget de charts de javascript para
    poder hacer las gráficas con datos que cambian y se selecciono hasta
    ahora el modo area, depende como vaya.
    
    creo q se tiene que mejorar la parte visual baja donde están el
    recuadro de iluminación y el de control de sliders para que tenga mejor
    vista.

--- public/js/chart.js
+++ public/js/chart.js
@@ -18,24 +18,40 @@ Highcharts.setOptions(Highcharts.theme);
 
         $('#container').highcharts({
             chart: {
-                type: 'line',
+                type: 'area',
                 animation: Highcharts.svg, // don't animate in old IE
                 marginRight: 10,
                 events: {
                     load: function () {
 
                         // set up the updating of the chart each second
-                        var series = this.series[0];
+                        var serie = this.series[0];
+                        var serie1 = this.series[1];
+                        var serie2 = this.series[2];
+                        var serie3 = this.series[3];
+                        var serie4 = this.series[4];
                         setInterval(function () {
                             var x = (new Date()).getTime(), // current time
-                                y = Math.floor((Math.random() * 10) + 1);
-                            series.addPoint([x, y], true, true);
-                        }, 2000);
+                                y = Math.floor((Math.random() * 32) + 20),
+                                y1= Math.floor((Math.random() * 32) + 20),
+                                y2= Math.floor((Math.random() * 32) + 20);
+                                y3= Math.floor((Math.random() * 32) + 20);
+                                y4= Math.floor((Math.random() * 32) + 20);
+
+                            serie.addPoint([x, y], true, true);
+                            serie1.addPoint([x, y1], true, true);
+                            serie2.addPoint([x, y2], true, true);
+                            serie3.addPoint([x, y3], true, true);
+                            serie4.addPoint([x, y4], true, true);
+
+                        }, 3000);
+
+                        
                     }
                 }
             },
             title: {
-                text: 'temperatura hogar'
+                text: 'syshome'
             },
             xAxis: {
                 type: 'datetime',
@@ -43,7 +59,7 @@ Highcharts.setOptions(Highcharts.theme);
             },
             yAxis: {
                 title: {
-                    text: 'grados c.'
+                    text: 'temperatura'
                 },
                 plotLines: [{
                     value: 0,
@@ -60,30 +76,95 @@ Highcharts.setOptions(Highcharts.theme);
             }
         },
             tooltip: {
-                formatter: function () {
-                    return '<b>' + this.series.name + '</b><br/>' +
-                        Highcharts.numberFormat(this.y, 2) + '<br/>' +
-                        Highcharts.dateFormat('%H:%M:%S', this.x);
-                }
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f}°C</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        
             },
             legend: {
-                enabled: false
+                enabled: true
             },
             exporting: {
                 enabled: false
             },
             series: [{
-                name: 'temp.',
+                name: 'sala',
                 data: (function () {
                     // generate an array of random data
                     var data = [],
                         time = (new Date()).getTime(),
                         i;
 
-                    for (i = -19; i <= 0; i += 1) {
+                    for (i = -15; i <= 0; i += 1) {
                         data.push({
                             x: time + i * 1000,
-                            y: Math.floor((Math.random() * 10) + 1)
+                            y: Math.floor((Math.random() * 35) + 20)
+                        });
+                    }
+                    return data;
+                }())
+            },{name: 'cuarto',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -15; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.floor((Math.random() * 35) + 20)
+                        });
+                    }
+                    return data;
+                }())
+            },
+            {name: 'cocina',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -15; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.floor((Math.random() * 35) + 20)
+                        });
+                    }
+                    return data;
+                }())
+            },
+            {name: 'lavanderia',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -15; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.floor((Math.random() * 35) + 20)
+                        });
+                    }
+                    return data;
+                }())
+            },
+            {name: 'patio',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -15; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.floor((Math.random() * 35) + 20)
                         });
                     }
                     return data;
--- public/js/slider.js
+++ public/js/slider.js
@@ -1,6 +1,14 @@
- var changeInput = document.querySelector('.js-check-change')
-      , initChangeInput = new Powerange(changeInput, { start: 70 });
+ var changeInput = document.querySelector('#chan1')
+      , initChangeInput = new Powerange(changeInput, { start: 50 });
 
     changeInput.onchange = function() {
-      document.getElementById('js-display-change').innerHTML = changeInput.value;
+      document.getElementById('change1').innerHTML = changeInput.value;
     };
+     var cambio = document.querySelector('#chan2')
+      , initcambio = new Powerange(cambio, { start: 50 });
+
+    cambio.onchange = function() {
+      document.getElementById('change2').innerHTML = cambio.value;
+    };
+
+
